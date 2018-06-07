7ba06a203bccb51cd2a8bb898c0e348c537f3503
crzyivo <ivan.nadie@gmail.com>
2018-06-07 18:10:39 +0200

    Recaptcha en el registro de usuarios.

--- app_server/controllers/users.js
+++ app_server/controllers/users.js
@@ -1,8 +1,12 @@
 const bdPath = require('../bdApiCalls');
+
 const nodemailer = require('nodemailer');
 const bcrypt = require('bcrypt');
 const CryptoJS = require('crypto-js');
 const randomstring = require('randomstring');
+const request = require('request');
+
+const secretoCaptcha = '6LcF1V0UAAAAAC0Ls1I-WLWJjcxtySnVzaKDDvcF';
 const ethmail = 'ucso5tvh2lduroxg@ethereal.email';
 const ethpass ='zfyZVHFC5rqU7uTtXJ';
 const transporter = nodemailer.createTransport({
@@ -15,44 +19,66 @@ const transporter = nodemailer.createTransport({
 });
 
 const postUsers = function(req,res){
-  bdPath.getUsuarios({email: req.body.email},
-      function (err,resBd,body) {
-        if(err){
-          res.status(500);
-          res.send(err);
-        }
-        if(body.message.length !== 0){
-          res.status(400).send("El usuario ya esta registrado");
-        }else{
-          var passGenerada = randomstring.generate({charset: 'wody',length: 10});
-          var usuario = req.body;
-          usuario.primerAcceso=true;
-          usuario.password = CryptoJS.SHA256(passGenerada).toString(CryptoJS.enc.Base64);
-          bcrypt.hash(usuario.password,5).then(function (hash) {
-            usuario.password = hash;
-            bdPath.postUsuarios(usuario,function (err,resPost,body){
-              if(err){
-                res.status(500);
-                res.send(err);
-              }
-              var mail = {
-                from: ethmail ,
-                to: usuario.email,
-                subject: 'Cuenta creada en WoodyTweetManager',
-                text: 'Se ha creado con exito su cuenta. Su contraseña es '+passGenerada+
-                '\nEsta contraseña es provisional, se le pedirá que cambie su contraseña cuando acceda por primera ' +
-                'vez a la aplicación',
-              };
-              transporter.sendMail(mail, function (err,info) {
-                if(err){console.log(err)}
-                console.log(info);
+  var googleCaptcha ={
+    'secret':secretoCaptcha,
+    'response':req.body.gRecaptcha
+  };
+  request.post({
+    uri: "https://www.google.com/recaptcha/api/siteverify",
+    form: googleCaptcha,
+    json:true
+  },function (err,resCaptcha,body) {
+    if (err) {
+      res.status(500)
+      .send(err);
+    }
+    if (!body.success) {
+      console.log(body["error-codes"]);
+      res.status(400).send(body["error-codes"]).end();
+    } else {
+
+      bdPath.getUsuarios({email: req.body.email},
+          function (err, resBd, body) {
+            if (err) {
+              res.status(500);
+              res.send(err);
+            }
+            if (body.message.length !== 0) {
+              res.status(400).send("El usuario ya esta registrado");
+            } else {
+              var passGenerada = randomstring.generate({charset: 'wody', length: 10});
+              var usuario = req.body;
+              usuario.primerAcceso = true;
+              usuario.password = CryptoJS.SHA256(passGenerada).toString(CryptoJS.enc.Base64);
+              bcrypt.hash(usuario.password, 5).then(function (hash) {
+                usuario.password = hash;
+                bdPath.postUsuarios(usuario, function (err, resPost, body) {
+                  if (err) {
+                    res.status(500);
+                    res.send(err);
+                  }
+                  var mail = {
+                    from: ethmail,
+                    to: usuario.email,
+                    subject: 'Cuenta creada en WoodyTweetManager',
+                    text: 'Se ha creado con exito su cuenta. Su contraseña es ' + passGenerada +
+                    '\nEsta contraseña es provisional, se le pedirá que cambie su contraseña cuando acceda por primera ' +
+                    'vez a la aplicación',
+                  };
+                  transporter.sendMail(mail, function (err, info) {
+                    if (err) {
+                      console.log(err)
+                    }
+                    console.log(info);
+                  });
+                  res.status(200);
+                  res.end();
+                });
               });
-              res.status(200);
-              res.end();
-            });
+            }
           });
-        }
-      });
+    }
+  });
 };
 
 const nuevaPass = function(req,res){
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,26 +1,40 @@
 // public/js/signup.js
 
-var regNg = angular.module('signup',[]);
+var regNg = angular.module('signup',['vcRecaptcha']);
 
 
-regNg.controller('registro',['$scope','$http','$window',function ($scope,$http,$window) {
+regNg.controller('registro',['$scope','$http','$window','vcRecaptchaService',function ($scope,$http,$window,$vcRecaptchaService) {
   $scope.datavalidate="Introduce un email válido";
-  $scope.mostrar = false;
+  $scope.exito = false;
+  $scope.introduceCaptcha = false;
   $scope.textoBoton = 'Crear cuenta';
   $scope.funcionBoton = 'nuevaCuenta()';
+  $scope.publicKey="6LcF1V0UAAAAAJ1_J88wxbd2FbGP1qbq3Rh22Ibi";
+
+  $scope.setWidget = function(widgetId){
+    $scope.widgetId = widgetId;
+  };
 
   $scope.nuevaCuenta = function() {
-    $http.post('/users', $scope.Datos)
-        .success(function (data) {
-          //OK
-          $scope.mostrar=true;
-          $scope.textoBoton='Logeate';
-          $scope.funcionBoton='vuelta()';
-        })
-        .error(function (data) {
-          $scope.datavalidate = "Este email ya esta en uso";
-          $scope.alert = true;
-        })
+    if($vcRecaptchaService.getResponse()===""){
+      $scope.introduceCaptcha=true;
+    }else {
+      $scope.Datos.gRecaptcha = $vcRecaptchaService.getResponse();
+      $http.post('/users', $scope.Datos)
+          .success(function (data) {
+            //OK
+            $scope.introduceCaptcha=false;
+            $scope.exito = true;
+            $scope.textoBoton = 'Logeate';
+            $scope.funcionBoton = 'vuelta()';
+          })
+          .error(function (data) {
+            $vcRecaptchaService.reload($scope.widgetId);
+            $scope.introduceCaptcha=false;
+            $scope.datavalidate = "Este email ya esta en uso";
+            $scope.alert = true;
+          })
+    }
   };
   $scope.vuelta = function () {
     $window.location.href('/');
